6088a834de3d60b19daf7a0dd233ab41005d0315
fariz1801 <33992679+fariz1801@users.noreply.github.com>
2017-11-26 08:45:22 +0700

    Create self.py

---
+++ self.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#Aan Team Bot
+
+import LINETCR
+from LINETCR.lib.curve.ttypes import *
+from datetime import datetime
+from bs4 import BeautifulSoup
+import time, random, sys, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, urllib2, wikipedia,tempfile,glob,shutil,unicodedata,goslate
+from gtts import gTTS
+
+#tinkerbell
+cl = LINETCR.LINE()
+cl.login(token="EnQDrcPaoE8mQjckxYx5.mfqHSZ7qrd+n+S8MhShxLq.jlzdqR1U8oKYUS7cv3pRsEq2WhkKy2JJE1AXme4eqjI=")
+cl.loginResult()
+#vidia
+kt = LINETCR.LINE()
+kt.login(token="EnZHxNFngISonGLWWdLd./YIqN25dLilBIZ7dOj8H+q.ZVOEABYBx4N6mklWq+SL7zQp2pExpC/7H0ENXfCExgQ=")
+kt.loginResult()
+#rosetta
+ks = LINETCR.LINE()
+ks.login(token="EnrYAjpbfsSrc48SUbSa.ePXpM3e81NKzdiuCjAexQG.nWqL9j0QfYOVG2O6vKbDdEA6wjq2eLzdEXxOINNJynQ=")
+ks.loginResult()
+#sirvelmist
+ki = LINETCR.LINE()
+ki.login(token="EmZS1vRoaP4tHIYcHz9b.okuULY4K/2zSGEX+A5EmsW.cTlLV1YKTYsV+msLhVfUbfAMN6TkW5uV03FyJ3+RiKk=")
+ki.loginResult()
+#fawn
+kk = LINETCR.LINE()
+kk.login(token="EmKINd3qUyOczqfYEO3c.vDGn1ogIA03Ty8TMAreYFa.EW7QQTa8MCcCu+pj9Arlw6HuKY9RShnhZIGZvA1nXQI=")
+kk.loginResult()
+#iridessa
+kc = LINETCR.LINE()
+kc.login(token="EmUB9VBu5yz0ADGJYn62.EaWVjkpkBZ9C1Ntwk6aEOG.iJTtPUnCettT8gENBF8HVe6ObNVbOb8wmrfvhEO1hqE=")
+kc.loginResult()
+
+print "DONE LOGIN"
+#print "T̐̄̈́̀̏͏̷̡̡͞e̱͍͍̟̯̞͙̙͍̜̯̅̈̍ͧͨ̽ͬ̾͏̶̶̵͡͝à̸̛̛͉̗̦͈͇̩̿̑ͯ̀̋͌̎̐ͯ̏ͤ͢͠m̨̦͔̠͈̜̫̖̘̗̠̽ͤͬͮͩ̋̑̽͛̌̉̿́̕ ͈҉̵̶̧̛̛͠͝҉̀͢͏̀͞ ̶̨̧̛̰̞͍͖͓͂̈́̄̃̇̐̐́̕̕͢͝ ̸̸̶̴̵̶̛̛̝͓̹̩̠̯̠̦̈̋̂̀̚͘͢͠͝͠J̶̛̲͇̤͔̝͕͍̮̳͎ͥ̾̐̆ͥ̑ͦ̓̓ͩ̎́ͅ͏̡̧̢̛̕̕͡͞u̵̵̲̽͑̆͌ͭ҉̵̧̧͘͝
